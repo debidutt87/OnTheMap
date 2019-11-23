89d4f65c09a24edc37edfad54291a178079dc81f
Debi <debidutt.prasad@nl.abnamro.com>
2019-11-23 01:11:29 +0100

    Initial Commit

---
+++ OnTheMap/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  OnTheMap
+//
+//  Created by Debidutt Prasad on 23/11/2019.
+//  Copyright © 2019 bot consultancy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
